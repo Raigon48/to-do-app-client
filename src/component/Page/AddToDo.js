9226b7bde679f0de43671b7910ea0e4d10ee54b9
Abhishek_Rai <raigon48@gmail.com>
2020-05-16 23:45:03 +0530

    added todo post function

--- src/component/Page/AddToDo.js
+++ src/component/Page/AddToDo.js
@@ -2,11 +2,13 @@ import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 
 import './AddToDo.css';
+import Axios from 'axios';
+import Loader from '../../util/Loading/Loading';
 
 function Todo(props) {
     const [title, setTitle ] = useState('');
     const [body, setBody] = useState('');
-    const [loading, setLoading] = useState('');
+    const [loading, setLoading] = useState(false);
 
     const handleChange = (event) => {
         const {name, value} = event.target;
@@ -20,19 +22,34 @@ function Todo(props) {
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        const sendData = {title, body}
+        setLoading(true);
+        const sendData = {title, body};
+        let token = localStorage.authToken;
+        Axios.defaults.headers.common = {Authorization : `${token}`};
+        Axios
+            .post('https://us-central1-wdatodoapplication.cloudfunctions.net/api/todo', sendData)
+            .then(response => {
+                console.log(response);
+                setLoading(false);
+            })
+            .catch(error => {
+                console.log(error);
+                setLoading(false);
+            })
     }
 
     return (
         <div id='create-container'>
-            <form id='todo-post'>
-                <label>Title : </label>
-                <input type='text' name='title' value={title} onChange={handleChange} placeholder='Enter title...' />
-                <label>Description : </label>
-                <textarea name='body' value={body} onChange={handleChange} placeholder='Enter Description...' />
-                <button onClick={handleSubmit}>Add ToDo</button>
-                <div><Link to='/todos'>Cancel</Link></div>
-            </form>
+            { loading ? 
+                <Loader/> :
+                <form id='todo-post'>
+                    <label>Title : </label>
+                    <input type='text' name='title' value={title} onChange={handleChange} placeholder='Enter title...' />
+                    <label>Description : </label>
+                    <textarea name='body' value={body} onChange={handleChange} placeholder='Enter Description...' />
+                    <button onClick={handleSubmit}>Add ToDo</button>
+                    <div><Link to='/todos'>Cancel</Link></div>
+                </form>}
         </div>
     )
 }
